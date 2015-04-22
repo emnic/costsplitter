ac842d6f93802e8fa81285c6df6a383f35a83ff2
emnic <emilnicklasson@hotmail.com>
2015-04-22 21:36:54 +0200

    Update with non functional active link

--- app.js
+++ app.js
@@ -5,35 +5,70 @@ codeSplitter.config(function ($routeProvider) {
     
         .when('/', {
             templateUrl: 'pages/add.html',
-            controller: 'mainController'
+            controller: 'navController'
         })
         .when('/add', {
             templateUrl: 'pages/add.html',
-            controller: 'addController'
+            controller: 'navController'
         })
         .when('/projects', {
             templateUrl: 'pages/projects.html',
-            controller: 'projectsController'
+            controller: 'navController'
         })
         .when('/contacts', {
             templateUrl: 'pages/contacts.html',
-            controller: 'contactsController'
+            controller: 'navController'
         })
 });
 
-codeSplitter.controller('mainController', ['$scope', '$log', function ($scope, $log) {
-
-
-}]);
-codeSplitter.controller('addController', ['$scope', '$log', function ($scope, $log) {
-    console.log("Hej");
-
-}]);
-codeSplitter.controller('projectsController', ['$scope', '$log', function ($scope, $log) {
-
+codeSplitter.controller('navController', ['$scope', '$log', '$location', function ($scope, $log, $location) {
+console.log($location.url())
+$scope.isActive = function (viewLocation) {
+  console.log(viewLocation)
+     var active = (viewLocation === $location.url());
+  console.log(active)
+     return active;
+};
 
 }]);
-codeSplitter.controller('contactsController', ['$scope', '$log', function ($scope, $log) {
 
+codeSplitter.directive('navMenu', function($location) {
+  console.log("hej")
+  return function(scope, element, attrs) {
+    var links = element.find('a'),
+        onClass = attrs.navMenu || 'on',
+        routePattern,
+        link,
+        url,
+        currentLink,
+        urlMap = {},
+        i;
 
-}]);
+    if (!$location.$$html5) {
+      routePattern = /^#[^/]*/;
+    }
+
+    for (i = 0; i < links.length; i++) {
+      link = angular.element(links[i]);
+      url = link.attr('href');
+
+      if ($location.$$html5) {
+        urlMap[url] = link;
+      } else {
+        urlMap[url.replace(routePattern, '')] = link;
+      }
+    }
+
+    scope.$on('$routeChangeStart', function() {
+      var pathLink = urlMap[$location.path()];
+
+      if (pathLink) {
+        if (currentLink) {
+          currentLink.removeClass(onClass);
+        }
+        currentLink = pathLink;
+        currentLink.addClass(onClass);
+      }
+    });
+  };
+});
