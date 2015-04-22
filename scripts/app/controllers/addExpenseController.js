aae2efe71aec65bae26efc0ff3848e0a87d2e1e4
Kalle Niklasson <kalle@aotp.se>
2015-04-23 00:12:06 +0200

    Ändringar  addexpense
    La till lite för att skapa nytt projekt vid inmatning av en utgift

--- scripts/app/controllers/addExpenseController.js
+++ scripts/app/controllers/addExpenseController.js
@@ -10,11 +10,22 @@
     function addExpenseController(resolvedExpenses) {
         /* jshint validthis:true */
         var vm = this;
+        vm.createNewProject = createNewProject;
+        vm.expense = { currency: 'kr' }
         vm.expenses = resolvedExpenses;
-        vm.expense = { currency: 'kr'}
+        vm.projects = [{ name: 'Ibiza', id: 1 }, { name: 'Fjällen', id: 2 }, { name: 'Svensexa', id: 3 }];
 
         activate();
 
         function activate() { }
+
+        function createNewProject() {
+            var newProject = {};
+            newProject.id = vm.projects[vm.projects.length - 1].id + 1;
+            newProject.name = vm.newProjectName;
+            vm.projects.push(newProject);
+            vm.expense.project = newProject;
+            vm.newProjectName = '';
+        }
     }
 })();
